adb61c40d99602fec5632f2bd573a1de133e8000
Jan Tichavský <tichavsk@seznam.cz>
2022-12-06 00:03:39 +0100

    examples improvment

--- src/LandingPage/Body/Examples.tsx
+++ src/LandingPage/Body/Examples.tsx
@@ -1,43 +1,75 @@
 import { Highlight } from "LandingPage/Components";
 import { spacingCss } from "react-utils/Components/globalCss";
-import { Column } from "react-utils/Components/StyledComponents";
+import { Column, Row } from "react-utils/Components/StyledComponents";
 import styled from "styled-components";
+import baseImg from "Assets/landing/sprites/base.png";
 
 export const Examples = () => {
     return (
         <StyledColumn>
-            <StyledTitle>{_("Case: Eshop")}</StyledTitle>
-            <p>
-                {_(
-                    "Image that you are providing e-shop. You have got over 1000 photos of your product that needs to be uploaded to your website, but you need to enhace them to match your requirements. You need to have every photo same exact size and the product placed right in the middle. You can do it all manually by our hands or you can use"
-                )}
-                <Highlight> Image pipeline!</Highlight>
-            </p>
-            <StyledTitle>{_("Case: Game sprites")}</StyledTitle>
-            <p>
-                {_(
-                    "You are creating a game, you found the greatest sprites for animation for your game, but you need to made background transparent and every animation frame is different size or is there some annoying padding. Create a"
-                )}
-                <Highlight> pipeline </Highlight>
-                {_(
-                    "that will do all the work for you and converts all your animation sheets into useable form."
-                )}
-            </p>
-            <StyledTitle>{_("Case: Neural network - dataset")}</StyledTitle>
-            <p>
-                {_(
-                    "You would like to train your own neural network. You already have some dataset of images but for better robustability you would like to generate some more data by mirroring, rotating or recoloring your dataset, so that your neural network will handle these variants as well. But how can you do that? You probably can use some downloaded script or write your script that will transform your data, but waht if there will be some more userfriendly solution such as"
-                )}
-                <Highlight> Image pipeline.</Highlight>
-            </p>
+            <StyledContainerBox>
+                <div>
+                    <StyledTitle>{_("Case: Eshop")}</StyledTitle>
+                    <p>
+                        {_(
+                            "Image that you are providing e-shop. You have got over 1000 photos of your product that needs to be uploaded to your website, but you need to enhace them to match your requirements. You need to have every photo same exact size and the product placed right in the middle. You can do it all manually by our hands or you can use"
+                        )}
+                        <Highlight> Image pipeline!</Highlight>
+                    </p>
+                </div>
+                <StyledImage src={baseImg} />
+            </StyledContainerBox>
+
+            <StyledContainerBox>
+                <StyledImage src={baseImg} />
+                <div>
+                    <StyledTitle>{_("Case: Game sprites")}</StyledTitle>
+                    <p>
+                        {_(
+                            "You are creating a game, you found the greatest sprites for animation for your game, but you need to made background transparent and every animation frame is different size or is there some annoying padding. Create a"
+                        )}
+                        <Highlight> pipeline </Highlight>
+                        {_(
+                            "that will do all the work for you and converts all your animation sheets into useable form."
+                        )}
+                    </p>
+                </div>
+            </StyledContainerBox>
+            <StyledContainerBox>
+                <div>
+                    <StyledTitle>{_("Case: Neural network - dataset")}</StyledTitle>
+                    <p>
+                        {_(
+                            "You would like to train your own neural network. You already have some dataset of images but for better robustability you would like to generate some more data by mirroring, rotating or recoloring your dataset, so that your neural network will handle these variants as well. But how can you do that? You probably can use some downloaded script or write your script that will transform your data, but what if there will be some more userfriendly solution such as"
+                        )}
+                        <Highlight> Image pipeline.</Highlight>
+                    </p>
+                </div>
+                <StyledImage src={baseImg} />
+            </StyledContainerBox>
         </StyledColumn>
     );
 };
 
 const StyledColumn = styled(Column)`
     padding: 0 ${spacingCss(3)};
+    gap: ${spacingCss(3)};
+    max-width: min(100%, 1000px);
+    margin: 0 auto;
 `;
 
 const StyledTitle = styled(Highlight)`
     font-size: 18px;
+`;
+
+const StyledContainerBox = styled(Row)`
+    border-radius: 12px;
+    background-color: rgba(255, 255, 255, 0.3);
+    padding: ${spacingCss(2)};
+    gap: ${spacingCss(10)};
+`;
+
+const StyledImage = styled.img`
+    width: 300px;
+    height: 200px;
 `;
